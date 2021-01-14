d2e0cfee658e71526fc2bc36ff50b71b4f10940d
prismsouza <pris.martins.souza@gmail.com>
2021-01-14 11:40:59 -0300

    getting layout better

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,3 +1,5 @@
+
+<div id="topo">
 <ul class="nav justify-content-center navbar-navy bg-navy p-1">
     <li class="nav-item">
         <a class="nav-link active text-white" href="http://intranet.bombeiros.mg.gov.br">INTRANET</a>
@@ -17,35 +19,37 @@
     <div class="row">
         <div class="col-md-2">
             <figure style="padding: 1em; background: #f8f9fa; border-radius: 5px; text-align: center;">
-                <a href="#"><img class="img-fluid" src={{ asset('images/logo.png') }} alt=""></a>
+                <a href="https://intranet.bombeiros.mg.gov.br"><img class="img-fluid" src={{ asset('images/logo.png') }} alt=""></a>
             </figure>
         </div>
         <div class="col-md-7">
             <h2 style="text-transform: uppercase; font-size: 1.2em; margin-top: 1em"><b>Pesquisa Normativa</b></h2>
             <p>Documentos Publicados do Corpo de Bombeiros Militar de Minas Gerais</p>
         </div>
-        <div class="col-md-2 py-5">
-            <div class="nav-item dropdown btn-light border " style="width: 130%">
+        <div class="col-md-3 form-inline">
+            <div class="nav-item dropdown btn-light border" >
                 <a class="nav-link text-secondary" href="#"
                    id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Links relacionados<span class="float-md-right"> <span class="caret"></span></span>
                 </a>
 
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item py-2" href="http://pesquisalegislativa.mg.gov.br/Legislacao.aspx" target="_blank">
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="width: 130%">
+                    <a class="dropdown-item py-2"
+                       href="http://pesquisalegislativa.mg.gov.br/Legislacao.aspx" target="_blank">
                         Normas Poder Executivo MG</a>
                     <a class="dropdown-item py-2" href="https://www.almg.gov.br/home/index.html" target="_blank">
                         Leis e Decretos Estaduais
                     </a>
                 </div>
             </div>
-        </div>
-        <div class="col-md-1 py-5">
-            <a href="{{ route('documents.refresh_session') }}" class="btn btn-light border float-md-right">
-                <i class="fas fa-home"></i>
-            </a>
+            <div>
+                <a href="{{ route('documents.refresh_session') }}" class="btn btn-light border float-md-right">
+                    <i class="fas fa-home"></i>
+                </a>
+            </div>
         </div>
     </div>
 
 </div>
 <br>
+</div>
--- resources/views/lateralmenu.blade.php
+++ resources/views/lateralmenu.blade.php
@@ -3,121 +3,143 @@ use App\Document;
 $categories = App\Category::all()->sortBy('name');
 $documents = Document::all();
 ?>
-
-<span>
-    <a class="list-group-item {{ Request::is('boletins') ? 'active' : ''}}"
-       href="{{route('boletins.refresh_session')}}">
-        BGBM + BEBM + Separata
-    </a>
-</span>
-
-<?php
-if (Session::has('categories') && count(Session::get('categories')) == 1 && !Request::is('boletins'))
-    $choosen_category = Session::get('categories')[0];
-else $choosen_category = null;
-?>
-
 <style>
-    .button {
-        cursor: pointer;
-        outline: none;
-        border: none;
+    @media screen and (min-width: 992px) {
+        #menulinks {
+            display: block;
+        }
+        #shmenulinks {
+            display: none;
+        }
     }
-    .buttonsub {
-        background: white;
-        padding: 6px 0px 6px 0px;
-        font-size: 14px;
-        text-align: center;
-        cursor: pointer;
-        outline: none;
-        color: dimgrey;
-        border: none;
+
+    @media screen and (max-width: 991px) {
+        #shmenulinks {
+            display: block;
+        }
     }
-    .buttonsub:hover {background-color: whitesmoke}
 </style>
-<form method="POST" action="{{ route('documents.index') }}" enctype="multipart/form-data" class="py-2"> @csrf
 
+<a id="shmenulinks" class="nav-link" style="border-radius: 0.25rem; cursor: pointer; width: 100%;" data-toggle="collapse" aria-expanded="true" aria-controls="menulinks" data-target="#menulinks"><i class="fa fa-bars" aria-hidden="true"></i></a>
+
+<div id="menulinks" class="nav-pills collapse">
     <ul class="nav nav-tabs flex-column lighten-4 list-group">
-        <li style="text-align: center">
-            <h3>Categorias</h3>
-        </li>
         <li class="nav-item border">
-            <a class="list-group-item" type="submit"
-               href="{{route('documents.refresh_session')}}">
-                <b>Todos</b>
-            </a>
+        <a class="list-group-item {{ Request::is('boletins') ? 'active' : ''}}"
+           href="{{route('boletins.refresh_session')}}">
+            BGBM / BEBM / Separata</a>
         </li>
+    </ul>
 
-        @foreach($categories as $category)
-            @if ($category->id == 1 ||  $category->id == 2|| $category->id == 3) @continue @endif
+    <?php
+    if (Session::has('categories') && count(Session::get('categories')) == 1 && !Request::is('boletins'))
+        $choosen_category = Session::get('categories')[0];
+    else $choosen_category = null;
+    ?>
+
+    <style>
+        .button {
+            cursor: pointer;
+            outline: none;
+            border: none;
+        }
+        .buttonsub {
+            background: white;
+            padding: 6px 0px 6px 0px;
+            font-size: 14px;
+            text-align: center;
+            cursor: pointer;
+            outline: none;
+            color: dimgrey;
+            border: none;
+        }
+        .buttonsub:hover {background-color: whitesmoke}
+    </style>
+    <form method="POST" action="{{ route('documents.index') }}" enctype="multipart/form-data" class="py-2"> @csrf
+
+        <ul class="nav nav-tabs flex-column lighten-4 list-group">
+            <li style="text-align: center">
+                <h3>Categorias</h3>
+            </li>
             <li class="nav-item border">
-                @if (count($category->hassubcategory)>0)
-                    <a class="collapsible button list-group-item" style="cursor: pointer">
-                        {{ $category->name }}
-                        <i class="fa fa-plus float-md-right"  style="color: lightblue" aria-hidden="true"></i>
-                    </a>
-                    <div class="content">
-                        <button type="submit" class="buttonsub {{ $choosen_category == $category->id ? 'active' : ''}}"
-                                name="categories[]" value="{{$category->id}}">
-                            <i class="fas fa-chevron-right fa-xs"></i>
-                            Todos
-                        </button>
-                        @foreach($category->hassubcategory as $sub_cat)<br>
-
-                        <button type="submit" class="buttonsub  {{ $choosen_category == $sub_cat->id ? 'active' : ''}}"
-                                name="categories[]" value="{{$sub_cat->id}}">
-                            <i class="fas fa-chevron-right fa-xs"></i>
-                            {{ $sub_cat->name }}
-                        </button>
-                        @endforeach
-                    </div>
-                @else
-                    @if (count($category->hasparent)==0)
-                        <button type="submit" class="button list-group-item {{ $choosen_category == $category->id  ? 'active' : ''}}"
-                                name="categories[]" value="{{$category->id}}">
-                            {{ $category->name }}
-                        </button>
-                    @endif
-                @endif
+                <a class="list-group-item" type="submit"
+                   href="{{route('documents.refresh_session')}}">
+                    <b>Todos</b>
+                </a>
             </li>
 
-        @endforeach
+            @foreach($categories as $category)
+                @if ($category->id == 1 ||  $category->id == 2|| $category->id == 3) @continue @endif
+                <li class="nav-item border">
+                    @if (count($category->hassubcategory)>0)
+                        <a class="collapsible button list-group-item" style="cursor: pointer">
+                            {{ $category->name }}
+                            <i class="fa fa-plus float-md-right"  style="color: lightblue" aria-hidden="true"></i>
+                        </a>
+                        <div class="content">
+                            <button type="submit" class="buttonsub {{ $choosen_category == $category->id ? 'active' : ''}}"
+                                    name="categories[]" value="{{$category->id}}">
+                                <i class="fas fa-chevron-right fa-xs"></i>
+                                Todos
+                            </button>
+                            @foreach($category->hassubcategory as $sub_cat)<br>
 
-    </ul>
+                            <button type="submit" class="buttonsub  {{ $choosen_category == $sub_cat->id ? 'active' : ''}}"
+                                    name="categories[]" value="{{$sub_cat->id}}">
+                                <i class="fas fa-chevron-right fa-xs"></i>
+                                {{ $sub_cat->name }}
+                            </button>
+                            @endforeach
+                        </div>
+                    @else
+                        @if (count($category->hasparent)==0)
+                            <button type="submit" class="button list-group-item {{ $choosen_category == $category->id  ? 'active' : ''}}"
+                                    name="categories[]" value="{{$category->id}}">
+                                {{ $category->name }}
+                            </button>
+                        @endif
+                    @endif
+                </li>
 
-    <ul class="nav nav-tabs flex-column lighten-4 list-group">
-        <li style="text-align: center">
-            <h3>Outros</h3>
-        </li>
-        <li class="nav-item border">
-            @if ($admin)
-                <span>
-                <a class="btn btn-sm" data-toggle="tooltip" title="visualizar"
-                   href="{{ route('ementario.view')}}" target="_blank">
-                    Ementário
-                </a>
-                <a href="{{ route('ementario.edit') }}" class="btn border float-md-right">
-                    <i class="fas fa-edit small" style="color: black"></i>
-                </a>
-                <a href="{{ route('ementario.download') }}" class="btn border float-md-right">
-                    <i class="fas fa-download small" style="color: black" aria-hidden="true"></i>
-                </a>
+            @endforeach
 
-            </span>
-            @else
-                <span>
-                <a class="btn" data-toggle="tooltip" title="visualizar"
-                   href="{{ route('ementario.view')}}" target="_blank">
-                    Ementário <i class="fas fa-eye"></i>
-                </a>
-            <a data-toggle="tooltip" title="download"
-               href="{{ route('ementario.download') }}" class="btn border float-md-right">
-                <i class="fas fa-download" style="color: black" aria-hidden="true"></i>
-            </a>
-            </span>
-            @endif
+        </ul>
 
-        </li>
-    </ul>
-</form>
+        <ul class="nav nav-tabs flex-column lighten-4 list-group">
+            <li style="text-align: center">
+                <h3>Outros</h3>
+            </li>
+            <li class="nav-item border">
+                @if ($admin)
+                    <span>
+                    <a class="btn btn-sm" data-toggle="tooltip" title="visualizar"
+                       href="{{ route('ementario.view')}}" target="_blank">
+                        Ementário
+                    </a>
+                    <a href="{{ route('ementario.edit') }}" class="btn border float-md-right">
+                        <i class="fas fa-edit small" style="color: black"></i>
+                    </a>
+                    <a href="{{ route('ementario.download') }}" class="btn border float-md-right">
+                        <i class="fas fa-download small" style="color: black" aria-hidden="true"></i>
+                    </a>
+
+                </span>
+                @else
+                    <span>
+                    <a class="btn" data-toggle="tooltip" title="visualizar"
+                       href="{{ route('ementario.view')}}" target="_blank">
+                        Ementário <i class="fas fa-eye"></i>
+                    </a>
+                <a data-toggle="tooltip" title="download"
+                   href="{{ route('ementario.download') }}" class="btn border float-md-right">
+                    <i class="fas fa-download" style="color: black" aria-hidden="true"></i>
+                </a>
+                </span>
+                @endif
+
+            </li>
+        </ul>
+    </form>
+</div>
 <script src="{{ asset('site/lateralmenu.js') }}"></script>
+
